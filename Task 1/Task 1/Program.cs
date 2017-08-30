6f0b671a747016a3b9fe48ac1de98224b2aa859c
User <User@WIN-4I8V0TPRSH5>
2017-08-30 21:18:44 +0300

    Init commit

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter count of numbers in array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Enter {0} number: ", i + 1);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Even numbers are: ");
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Console.Write(array[i] + " ");
+                }                   
+            }
+            Console.Write("\nOdd numbers are: ");
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    Console.Write(array[i] + " ");
+                }                    
+            }
+            Console.ReadLine();
+        }
+    }
+}
